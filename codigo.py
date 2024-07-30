b92d819fabf0132d663c66f1095396e589fb336e
Vitor1017 <vitor.alves.miranda@hotmail.com>
2024-07-29 22:44:33 -0300

    gastos

---
+++ codigo.py
@@ -0,0 +1,13 @@
+Salario = 1500
+Aluguel = 400
+Alimentação = 500
+Academia = 100
+Internet = 120
+
+print('Gastos Mensais ')
+print('Aluguel', Aluguel)
+print('Alimentação', Alimentação)
+print('Academia', Academia)
+print('Internet', Internet)
+print('Sobra', Salario - Aluguel - Alimentação - Academia - Internet)
+
